cf9a63c514b6232acd3a83fbcb84094a1e004143
Alperen Dagli <alperen.dagli43@gmail.com>
2024-05-16 21:00:47 +0200

    bla

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/projects", projectsRoute);
 app.use("/issues", issuesRoute);
 app.use("/rules", rulesRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
